312e025f3a5dda3a34d704799e48cd1ba104a1a8
dougcampbell66 <64913709+dougcampbell66@users.noreply.github.com>
2020-10-27 08:21:08 -0400

    fixxbuzz

---
+++ FizzBuzzXeril/FizzBuzz.js
@@ -0,0 +1,29 @@
+﻿//Solution 1: Most common way to solve FizzBuzz
+function FizzBuzzA(value1, value2) {
+    let returnValue = "";
+    for(let i = 1; i <= 100; i++) {
+        if (i%value1 == 0 && i%value2 == 0) {
+            returnValue += 'FizzBuzz ';
+        }
+        else if (i%value1 == 0){
+            returnValue += 'Fizz '
+        }
+        else if (i%value2==0){
+            returnValue += 'Buzz '
+        }
+        else{
+            returnValue += i ' ';
+        }
+    }
+    return returnValue;
+}
+//Call and use FizzBuzz Function
+function buzzIt(){
+    let output="";
+    let val1 = document.getElementById('Fizzvalue').value;
+    let val2 = document.getElementById('Buzzvalue').value;
+    output = FizzBuzzA(val, val2);
+    document.getElementById('results').innerHTML = output;
+}
+
+
